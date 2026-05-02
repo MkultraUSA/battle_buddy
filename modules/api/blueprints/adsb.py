eb6c2ed074dafc9b4be4b827b39007c2678b364e
Hermes <hermes@battlebuddy.new>
2026-05-02 14:18:17 +0000

    Revert broken modularization

--- modules/api/blueprints/adsb.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from modules.database import get_adsb_map_points
-
-bp = Blueprint('adsb', __name__)
-
-
-@bp.route("/api/adsb")
-def api_adsb():
-    return jsonify(get_adsb_map_points())
--- modules/api/blueprints/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, redirect
-
-bp = Blueprint('main', __name__)
-
-
-@bp.route("/")
-def index():
-    return redirect("/static/index.html", code=302)
